038a6d0d095953d7bebcd95c78d987229c92a53a
xiaoliujava <1390135253@qq.com>
2020-11-03 10:18:26 +0800

    本地仓库接受

---
+++ ssm/src/com/liuchunming/Demo1.java
@@ -0,0 +1,13 @@
+package com.liuchunming;
+
+/**
+ * @author liuchunming
+ * @site www.liuchunming.com
+ * @company xxx公司
+ * @create  2020-11-03 10:15
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(" idea git demo ");
+    }
+}
